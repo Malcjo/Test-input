4bb4ccef3b3eb2b4b1e842dd7611da11cdbb1154
Josh <joshuaeric.malcolm@gmail.com>
2021-02-18 15:29:49 +1300

    second commit

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 5;
+    private Vector2 Input;
+    [SerializeField] private int playerIndex = 0;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 DisplayVelocity;
+    public void Test()
+    {
+        Debug.Log("Test Jump: " + GetPlayerIndex());
+    }
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+    public void SetPlayerIndex(int var)
+    {
+        playerIndex = var;
+    }
+    public void SetInput(Vector2 direction)
+    {
+        Input = direction;
+    }
+    private void Update()
+    {
+
+        rb.velocity = new Vector3(Input.x, 0, Input.y)*speed;
+        DisplayVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    }
+}
---
+++ Assets/PlayerInputHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private Player player;
+    [SerializeField] private int PlayerIndex;
+    private void Awake()
+    {
+        var Index = playerInput.playerIndex;
+        PlayerIndex = playerInput.playerIndex;
+        player.SetPlayerIndex(playerInput.playerIndex);
+    }
+
+    public void OnJump(CallbackContext context)
+    {
+        if(player != null)
+        {
+            player.Test();
+        }
+    }
+    public void OnMove(CallbackContext context)
+    {
+        if(player != null)
+        {
+            player.SetInput(context.ReadValue<Vector2>());
+        }
+    }
+}
